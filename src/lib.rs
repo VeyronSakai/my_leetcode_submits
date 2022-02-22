5d1da228339c6fdcceadcfa6fba5b6d1a7c396b2
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-23 04:51:43 +0900

    problem21

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ mod problem9_palindrome_number;
 mod problem13;
 mod problem14;
 mod problem20;
+mod problem21;
 mod problem68;
 mod problem150;
 mod problem359;
+
 
 
 pub struct Solution;
---
+++ src/problem21.rs
@@ -0,0 +1,85 @@
+use crate::Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+impl Solution{
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (list1, list2) {
+            (None, None) => None,
+            (Some(node), None) | (None, Some(node)) => Some(node),
+            (Some(node1), Some(node2)) => {
+                if node1.val >= node2.val {
+                    Some(Box::new(ListNode {
+                        val: node2.val,
+                        next: Solution::merge_two_lists(Some(node1), node2.next),
+                    }))
+                } else {
+                    Some(Box::new(ListNode {
+                        val: node1.val,
+                        next: Solution::merge_two_lists(node1.next, Some(node2)),
+                    }))
+                }
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use test_macro::*;
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let mut arg1_1 = ListNode::new(1);
+        let mut arg1_2 = ListNode::new(2);
+        let mut arg1_3 = ListNode::new(4);
+
+        arg1_3.next = None;
+        arg1_2.next = Some(Box::new(arg1_3));
+        arg1_1.next = Some(Box::new(arg1_2));
+
+        let mut arg2_1 = ListNode::new(1);
+        let mut arg2_2 = ListNode::new(3);
+        let mut arg2_3 = ListNode::new(4);
+
+        arg2_3.next = None;
+        arg2_2.next = Some(Box::new(arg2_3));
+        arg2_1.next = Some(Box::new(arg2_2));
+
+        let mut ret1 = ListNode::new(1);
+        let mut ret2 = ListNode::new(1);
+        let mut ret3 = ListNode::new(2);
+        let mut ret4 = ListNode::new(3);
+        let mut ret5 = ListNode::new(4);
+        let mut ret6 = ListNode::new(4);
+
+        ret6.next = None;
+        ret5.next = Some(Box::new(ret6));
+        ret4.next = Some(Box::new(ret5));
+        ret3.next = Some(Box::new(ret4));
+        ret2.next = Some(Box::new(ret3));
+        ret1.next = Some(Box::new(ret2));
+
+        assert_eq!(Solution::merge_two_lists(Some(Box::new(arg1_1)), Some(Box::new(arg2_1))), Some(Box::new(ret1)));
+    }
+
+    test_macro::test_assert_eq!(example2, Solution::merge_two_lists(None, None) => None);
+    test_macro::test_assert_eq!(example3, Solution::merge_two_lists(None, Some(Box::new(ListNode::new(0)))) => Some(Box::new(ListNode::new(0))));
+}
