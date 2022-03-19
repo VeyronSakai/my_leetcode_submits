92736ec115a65afea5580a3d41022f30863d96dd
Yuki Sakai <yuukisakai123@gmail.com>
2022-03-19 15:05:30 +0900

    problem252

---
+++ src/problem252.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+struct Solution;
+
+impl Solution {
+    pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> bool {
+        let mut set = HashSet::new();
+
+        for interval in &intervals {
+            for val in interval[0]..interval[1] {
+                if set.contains(&val) {
+                    return false;
+                }
+
+                set.insert(val);
+            }
+        }
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        assert_eq!(Solution::can_attend_meetings(vec![vec![0, 30], vec![5, 10], vec![15, 20]]), false);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(Solution::can_attend_meetings(vec![vec![7, 10], vec![2, 4]]), true);
+    }
+}
