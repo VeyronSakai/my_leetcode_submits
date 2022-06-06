8c51832c7213481f857863cc63a981641de70169
Yuki Sakai <yuukisakai123@gmail.com>
2022-06-07 07:52:07 +0900

    review problem2

--- src/problem2.rs
+++ src/problem2.rs
@@ -1,4 +1,4 @@
-use crate::Solution;
+use crate::problem1305::TreeNode;
 
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -19,24 +19,65 @@ impl ListNode {
 
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        match (l1, l2) {
-            (None, None) => None,
-            (Some(n), None) | (None, Some(n)) => Some(n),
-            (Some(n1), Some(n2)) => {
-                let sum = n1.val + n2.val;
-                if sum < 10 {
-                    Some(Box::new(ListNode {
-                        val: sum,
-                        next: Solution::add_two_numbers(n1.next, n2.next),
-                    }))
-                } else {
-                    let carry = Some(Box::new(ListNode::new(1)));
+        return Self::helper(&l1, &l2, 0);
+    }
+
+    fn helper(l1: &Option<Box<ListNode>>, l2: &Option<Box<ListNode>>, carry: i32) -> Option<Box<ListNode>> {
+        return match (l1, l2) {
+            (Some(l1), Some(l2)) => {
+                let sum = l1.val + l2.val + carry;
+                if sum > 9 {
                     Some(Box::new(ListNode {
                         val: sum - 10,
-                        next: Solution::add_two_numbers(Solution::add_two_numbers(carry, n1.next), n2.next),
+                        next: Self::helper(&l1.next, &l2.next, 1),
+                    }))
+                } else {
+                    Some(Box::new(ListNode {
+                        val: sum,
+                        next: Self::helper(&l1.next, &l2.next, 0),
                     }))
                 }
             }
-        }
+            (Some(l), None) => {
+                let sum = l.val + carry;
+                if sum > 9 {
+                    Some(Box::new(ListNode {
+                        val: sum - 10,
+                        next: Self::helper(&l.next, &None, 1),
+                    }))
+                } else {
+                    Some(Box::new(ListNode {
+                        val: sum,
+                        next: Self::helper(&l.next, &None, 0),
+                    }))
+                }
+            }
+            (None, Some(l)) => {
+                let sum = l.val + carry;
+                if sum > 9 {
+                    Some(Box::new(ListNode {
+                        val: sum - 10,
+                        next: Self::helper(&None, &l.next, 1),
+                    }))
+                } else {
+                    Some(Box::new(ListNode {
+                        val: sum,
+                        next: Self::helper(&None, &l.next, 0),
+                    }))
+                }
+            }
+            (None, None) => {
+                if carry == 1 {
+                    Some(Box::new(ListNode {
+                        val: 1,
+                        next: None,
+                    }))
+                } else {
+                    None
+                }
+            }
+        };
     }
 }
+
+struct Solution;
