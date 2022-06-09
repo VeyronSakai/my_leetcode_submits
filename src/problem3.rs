851dfbc3c87e24acfa33f0a021aed5849ce4c062
Yuki Sakai <yuukisakai123@gmail.com>
2022-06-10 05:52:18 +0900

    refactor problem3

--- src/problem3.rs
+++ src/problem3.rs
@@ -1,6 +1,4 @@
-use std::cmp::max;
 use std::collections::HashMap;
-use crate::Solution;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
@@ -10,27 +8,30 @@ impl Solution {
 
         let mut ret = 0;
 
-        let chars = s.chars().collect::<Vec<char>>();
-        let chars_len = chars.len();
-
-        for i in 0..chars_len {
-            if map.contains_key(&chars[i]) {
-                left = max(*map.get(&chars[i]).unwrap() as usize + 1, left);
+        for (i, c) in s.chars().enumerate() {
+            if map.contains_key(&c) {
+                left = left.max(*map.get(&c).unwrap() as usize + 1);
             }
 
-            map.insert(chars[i], i);
-            ret = max(ret, i - left + 1);
+            map.insert(c, i);
+            ret = ret.max(i - left + 1);
         }
 
         ret as i32
     }
 }
 
+struct Solution;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    test_macro::test_assert_eq!(example1, Solution::length_of_longest_substring("abcabcbb".to_string()) => 3);
+    #[test]
+    fn example1() {
+        assert_eq!(Solution::length_of_longest_substring("abcabcbb".to_string()), 3);
+    }
+
     test_macro::test_assert_eq!(example2, Solution::length_of_longest_substring("bbbbb".to_string()) => 1);
     test_macro::test_assert_eq!(example3, Solution::length_of_longest_substring("pwwkew".to_string()) => 3);
     test_macro::test_assert_eq!(test1, Solution::length_of_longest_substring("aab".to_string()) => 2);
